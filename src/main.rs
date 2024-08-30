e8011d6e253ff27728aa138f66605ff35a9b3d7b
Rana718 <ranadolui718@gmail.com>
2024-08-30 11:46:34 +0530

    update the code

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,21 @@
 mod commands;
+mod godx_commands;
 
-use clap::{Arg, Command};
+use clap::{Command, ArgMatches};
+use godx_commands::basic_commands;
 
 fn main() {
     let matches = Command::new("godx")
         .version("1.0")
         .author("Rana Dolui <your.email@example.com>")
         .about("A simple Git command wrapper")
-        .subcommand(
-            Command::new("add")
-                .alias("-a")
-                .about("Adds a file to staging")
-                .arg(Arg::new("filename").required(true))
-        )
-        .subcommand(
-            Command::new("commit")
-                .alias("com")
-                .about("Commits staged changes with a message")
-                .arg(Arg::new("message").required(true))
-        )
-        .subcommand(
-            Command::new("status")
-                .alias("stat")
-                .about("Shows the current status of the repository")
-        )
-        .subcommand(
-            Command::new("remote")
-                .alias("-r")
-                .about("Add remote repository")
-                .arg(Arg::new("url").required(true))
-        )
-        .subcommand(
-            Command::new("push")
-                .about("Pushes changes to the remote repository")
-        )
-        .subcommand(
-            Command::new("branch")
-                .alias("-b")
-                .about("Renames the current branch")
-                .arg(Arg::new("name").required(true).short('m'))
-        )
+        .subcommands(basic_commands::all_commands())
         .get_matches();
+    
+    handle_matches(matches);
+}
 
+fn handle_matches(matches: ArgMatches) {
     if let Some(matches) = matches.subcommand_matches("add") {
         let filename = matches.get_one::<String>("filename").unwrap();
         commands::run_add_command(filename);
@@ -58,5 +32,78 @@ fn main() {
     } else if let Some(matches) = matches.subcommand_matches("branch") {
         let name = matches.get_one::<String>("name").unwrap();
         commands::run_branch_command(name);
+    } else if let Some(matches) = matches.subcommand_matches("checkout") {
+        let branch = matches.get_one::<String>("branch").unwrap();
+        commands::run_checkout_command(branch);
+    } else if let Some(matches) = matches.subcommand_matches("merge") {
+        let branch = matches.get_one::<String>("branch").unwrap();
+        commands::run_merge_command(branch);
+    } else if matches.subcommand_matches("log").is_some() {
+        commands::run_log_command();
+    } else if matches.subcommand_matches("diff").is_some() {
+        commands::run_diff_command();
+    } else if let Some(matches) = matches.subcommand_matches("reset") {
+        let commit = matches.get_one::<String>("commit").unwrap();
+        commands::run_reset_command(commit);
+    } else if matches.subcommand_matches("pop").is_some() {
+        commands::run_stash_pop_command();
+    } else if matches.subcommand_matches("pull").is_some() { 
+        commands::run_pull_command();
     }
 }
+
+
+// fn main() {
+//     let matches = Command::new("godx")
+//         .version("1.0")
+//         .author("Rana Dolui <your.email@example.com>")
+//         .about("A simple Git command wrapper")
+//         .subcommand(basic_commands::add_command())
+//         .subcommand(basic_commands::commit_command())
+//         .subcommand(basic_commands::init_command())
+//         .subcommand(basic_commands::status_command())
+//         .subcommand(basic_commands::remote_command())
+//         .subcommand(basic_commands::push_command())
+//         .subcommand(basic_commands::branch_command())
+//         .subcommand(basic_commands::checkout_command())
+//         .subcommand(basic_commands::merge_command())
+//         .subcommand(basic_commands::log_command())
+//         .subcommand(basic_commands::diff_command())
+//         .subcommand(basic_commands::reset_command())
+//         .subcommand(basic_commands::pop_stash_command())
+//         .get_matches();
+
+//     if let Some(matches) = matches.subcommand_matches("add") {
+//         let filename = matches.get_one::<String>("filename").unwrap();
+//         commands::run_add_command(filename);
+//     } else if let Some(matches) = matches.subcommand_matches("commit") {
+//         let message = matches.get_one::<String>("message").unwrap();
+//         commands::run_commit_command(message);
+//     } else if matches.subcommand_matches("status").is_some() {
+//         commands::run_status_command();
+//     } else if let Some(matches) = matches.subcommand_matches("remote") {
+//         let url = matches.get_one::<String>("url").unwrap();
+//         commands::run_remote_add_command(url);
+//     } else if matches.subcommand_matches("push").is_some() {
+//         commands::run_push_command();
+//     } else if let Some(matches) = matches.subcommand_matches("branch") {
+//         let name = matches.get_one::<String>("name").unwrap();
+//         commands::run_branch_command(name);
+//     } else if let Some(matches) = matches.subcommand_matches("checkout") {
+//         let branch = matches.get_one::<String>("branch").unwrap();
+//         commands::run_checkout_command(branch);
+//     } else if let Some(matches) = matches.subcommand_matches("merge") {
+//         let branch = matches.get_one::<String>("branch").unwrap();
+//         commands::run_merge_command(branch);
+//     } else if matches.subcommand_matches("log").is_some() {
+//         commands::run_log_command();
+//     } else if matches.subcommand_matches("diff").is_some() {
+//         commands::run_diff_command();
+//     } else if let Some(matches) = matches.subcommand_matches("reset") {
+//         let commit = matches.get_one::<String>("commit").unwrap();
+//         commands::run_reset_command(commit);
+//     }
+//      else if matches.subcommand_matches("pop").is_some() {
+//         commands::run_stash_pop_command();
+//     }
+// }
